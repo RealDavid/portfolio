d24d305e8534ba070a7e53f321e930232779598f
David <davidmusngzi@gmail.com>
2024-07-18 13:03:23 +0300

    3.4 Birthday Invite Project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,24 @@
 function App() {
   return (
     <>
-    {/* <!-- Kitten image URL --> */}
-    <h1>Im a cat person</h1>
-    <img src="https://raw.githubusercontent.com/appbrewery/webdev/main/kitten.jpeg
-" alt="Picture of cute cat" />
 
+    <h1>It's My Birthday</h1>
+    <h2>On August 28th</h2>
+    {/* <!-- Example image URL --> */}
+    <img src="https://raw.githubusercontent.com/appbrewery/webdev/main/birthday-cake3.4.jpeg
+    " alt="A picture of a birth cake" />
 
-{/* <!-- Puppy image URL --> */}
-{/* https://raw.githubusercontent.com/appbrewery/webdev/main/puppy.gif */}
+    <h3>What to bring</h3>
+    <ul>
+      <li>Cake</li>
+      <li>Presents</li>
+      <li>Toy Robot</li>
 
+    </ul>
+
+{/* <!-- Example Google Maps Link --> */}
+    <h3>Where to go</h3>
+<a href="https://www.google.com/maps/@35.7040744,139.5577317,3a,75y,289.6h,87.01t,0.72r/data=!3m6!1e1!3m4!1sgT28ssf0BB2LxZ63JNcL1w!2e0!7i13312!8i6656">Location link</a>
     </>
   )
 }
